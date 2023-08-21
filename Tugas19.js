6432fe04027599596c5f9a832ac33a8ab967a8b9
felster527 <91239878+felster527@users.noreply.github.com>
2023-08-21 19:33:59 +0700

    Add files via upload

---
+++ Tugas19.js
@@ -0,0 +1,16 @@
+var Repeat = ()=> {
+    var kalimat = "Semangat..."
+    var asterisk = "*****"
+    console.log(kalimat.repeat(5))
+    console.log(asterisk)
+    console.log(asterisk.repeat(5))
+    console.log(asterisk.repeat(9))
+    console.log(asterisk.repeat(12))    
+    console.log(asterisk.repeat(17))
+    console.log(asterisk.repeat(12))
+    console.log(asterisk.repeat(9))
+    console.log(asterisk.repeat(5))
+    console.log(asterisk)
+    
+}
+Repeat()
